c6119f2d1b00197da30b4196c5b584a1ba12d684
YuZhang-SMU <87842101+YuZhang-SMU@users.noreply.github.com>
2023-02-01 20:49:05 +0800

    Delete preprocess directory

--- preprocess/LSC_low/Normalize.m
+++
@@ -1,6 +0,0 @@
-function nor_label = Normalize(InImg)
-  ymax=255;ymin=0;
-  xmax = max(max(InImg));
-  xmin = min(min(InImg));
-  nor_label = round((ymax-ymin)*(InImg-xmin)/(xmax-xmin) + ymin);
-end
